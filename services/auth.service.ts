eb9c5e6ca5f7c6ff0034b77d4c58fa1a80e3bb73
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2024-01-02 10:09:44 +0200

    Small fix

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -189,7 +189,7 @@ export default class AuthService extends moleculer.Service {
     });
 
     if (authUserGroups && authUserGroups.length && user?.id) {
-      const authGroups = authUserGroups.filter((g) => g.id != NSA_GROUP_ID && !g.companyCode);
+      const authGroups = authUserGroups.filter((g) => g.id != NSA_GROUP_ID && !!g.companyCode);
 
       for (const group of authGroups) {
         await ctx.call('tenantUsers.createRelationshipsIfNeeded', {
